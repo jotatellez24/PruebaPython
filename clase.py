1cf495eef6afe6a7851fd207fbba25ea03883e4a
JOTA <jotatellez24@gmail.com>
2023-04-21 21:34:21 -0500

    Primer Commit

---
+++ clase.py
@@ -0,0 +1,23 @@
+def is_prime(n):
+    """Determina si un número entero es primo."""
+    if n <= 1:
+        return False
+    elif n <= 3:
+        return True
+    elif n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+def fibonacci(n):
+    """Devuelve el número Fibonacci correspondiente a la posición n."""
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
---
+++ test_fibonacci.py
@@ -0,0 +1,39 @@
+#################################
+import pytest  
+from clase import *
+def test_is_prime():
+    assert is_prime(0)==False
+    assert is_prime(1)==False 
+    assert is_prime(2)==True 
+    assert is_prime(3)==True 
+    assert is_prime(4)==False
+ 
+#Falla
+#  def test_is_prime():
+#     assert is_prime(0)==True
+#     assert is_prime(1)==False 
+#     assert is_prime(2)==True 
+#     assert is_prime(3)==True 
+#     assert is_prime(4)==False
+  
+#Falla
+# def test_fibonacci():
+#     assert fibonacci(1) == 2
+#     assert fibonacci(2) == 1
+#     assert fibonacci(3) == 2
+#     assert fibonacci(4) == 3
+#     assert fibonacci(5) == 5
+#     assert fibonacci(6) == 8
+#     assert fibonacci(7) == 13
+#     assert fibonacci(8) == 21
+
+##################################
+def test_fibonacci():
+    assert fibonacci(1) == 1
+    assert fibonacci(2) == 1
+    assert fibonacci(3) == 2
+    assert fibonacci(4) == 3
+    assert fibonacci(5) == 5
+    assert fibonacci(6) == 8
+    assert fibonacci(7) == 13
+    assert fibonacci(8) == 21
